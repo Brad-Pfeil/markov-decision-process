8088b075c9dd7a08f08d5b5e86e9fb271bd50493
Brad Pfeil <Brad-Pfeil@users.noreply.github.com>
2025-03-09 16:53:00 +1030

    feat: working example_1

--- src/markov_decision_process/time_augmented_mdp.py
+++ src/markov_decision_process/time_augmented_mdp.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import inspect
+import copy
 import logging
 
 logger = logging.getLogger(__name__)
@@ -35,6 +36,7 @@ class TimeAugmentedMDP:
         mode: Literal['flexible', 'vectorized', 'model'] | None = None,
         model: Callable | None = None,
         state_space_data_path: str | None = None,
+        force_overwrite: bool = False,
     ):
 
         #---------------------------------------------------------------------
@@ -43,11 +45,13 @@ class TimeAugmentedMDP:
         # Assign these inputs to self
         for key, value in locals().items():
             if key != 'self':
-                setattr(self, key, value)
+                # Set a copy, otherwise we'll mutate the original
+                setattr(self, key, copy.deepcopy(value))
 
         # Transitions and rewards will be constructed by methods below
         self.transition_matrices: List[csr_matrix] = []
         self.reward_matrices: List[csr_matrix] = []
+        self.INFEASIBLE = -1e8
 
         # Augmented states
         self.states_augmented: List[Tuple] = []
@@ -68,7 +72,7 @@ class TimeAugmentedMDP:
 
         #---------------------------------------------------------------------
         # Augmentation
-
+    
         # This mutates the times list to add an additional dummy time,
         # creates a list called states_augmented, and a dictionary called
         # augmented_state_to_index.
@@ -77,7 +81,7 @@ class TimeAugmentedMDP:
         #---------------------------------------------------------------------
 
         # Generate the state space data
-        self.data = self.__generate_state_space_data()
+        self.state_space_data = self.__generate_state_space_data()
 
 
         return None
@@ -229,23 +233,29 @@ class TimeAugmentedMDP:
         # Check if a path has been passed
         if self.state_space_data_path is not None:
             state_space_path = self.state_space_data_path
-
-            # Check if there's data there
-            try: 
-                data = pl.scan_parquet(state_space_path)
-                data.head().collect()
-                logger.info("Data found on disk. Loading in...")
-                return data
-            except:
-                logger.info("No data found on disk. Generating...")
         else:
             state_space_path  = '/tmp/state_space_data/'
             logger.info("No path provided. Saving to /tmp/state_space_data/")
 
             # Check if the directory exists
-            if os.path.exists(state_space_path):
+            if os.path.exists(state_space_path) and not self.force_overwrite:
                 raise ValueError(f"Directory {state_space_path} already exists. Please provide a new path or delete the existing directory.")
-                
+
+        # If force_overwrite is True, delete the directory
+        # and recreate it.
+        if self.force_overwrite:
+            logger.warn("Force overwrite active. Deleting existing directory and recreating...")
+            os.system(f"rm -rf {state_space_path}")
+            os.makedirs(state_space_path, exist_ok=True)    
+            
+        # Check if the data already exists on disk
+        try: 
+            data = pl.scan_parquet(state_space_path)
+            data.head().collect()
+            logger.info("Data found on disk. Loading in...")
+            return data
+        except:
+            logger.info("No data found on disk. Generating...")
 
         # Create the data
 
@@ -283,3 +293,457 @@ class TimeAugmentedMDP:
         data = pl.scan_parquet(state_space_path)
 
         return data
+    
+    def __build_matrix(
+        self,
+        df: pl.DataFrame,
+    ) -> csr_matrix:
+        """
+        Given a data frame with columns s_prime, s, t, t_prime, probability,
+        and reward, build a transition or reward matrix.
+
+        Parameters:
+        -----------
+        df: pl.DataFrame
+            A data frame with columns s_prime, s, t, t_prime, probability, and
+            reward.
+
+        Returns:
+        --------
+        csr_matrix:
+            A sparse matrix representing the transition or reward matrix.
+        """
+
+        # Each row has s_prime, s, t, t_prime. Add two new columnns,
+        # s_augmented_index and s_prime_augmented_index. These are the indices
+        # of the augmented state space for tuples (s,t) and (s_prime, t_prime).
+        
+        # Create new columns by applying the mapping
+        df = df.with_columns([
+            pl.struct(["s", "t"])
+            .map_elements(
+                lambda row: self.augmented_state_to_index.get((row["s"], row["t"]), -1),
+                return_dtype=pl.UInt64,
+            )
+            .alias("s_augmented_index")
+        ])
+        df = df.with_columns([
+            pl.struct(["s_prime", "t_prime"])
+            .map_elements(
+                lambda row: self.augmented_state_to_index.get((row["s_prime"], row["t_prime"]), -1),
+                return_dtype=pl.UInt64,
+            )
+            .alias("s_prime_augmented_index")
+        ])
+
+        # Create a sparse matrix with dimension len(S_augmented) x
+        # len(S_augmented). Note that we need the indices in S_augmented
+        # corresponding to (s, t) and (s_prime, t_prime).
+        # Create a column called 'index' for what the index of (s,t) is.
+
+        P = csr_matrix(
+            (
+                df["probability"],
+                (
+                    df["s_augmented_index"],
+                    df["s_prime_augmented_index"],
+                ),
+            ),
+            shape=(len(self.states_augmented), len(self.states_augmented)),
+            dtype=float,
+        )
+
+        R = csr_matrix(
+            (
+                df["reward"],
+                (
+                    df["s_augmented_index"],
+                    df["s_prime_augmented_index"],
+                ),
+            ),
+            shape=(len(self.states_augmented), len(self.states_augmented)),
+            dtype=float,
+        )
+
+        return P, R
+
+    def build_rewards_and_transitions(self):
+        """
+        Generate the reward and transition matrices.
+
+        If mode is 'flexible' or 'vectorized', we'll use the reward and
+        transition functions to generate these matrices.
+
+        If mode is 'model', we'll use the model to generate these matrices.
+        """
+
+        logger.info("Generating rewards and transitions...")
+
+        transitions = []
+        rewards = []
+
+        for a in self.actions:
+            
+            # Filter down to the subset of df with action a
+            df_a = self.state_space_data.filter(pl.col('a') == a)
+
+            if self.mode == 'flexible':
+                # Use the apply method to apply the transition and reward
+                # functions
+                df_a = df_a.with_columns([
+                    pl.struct('s_prime', 's', 'a', 't').map_elements(
+                        lambda row: self.transition_function(row['s_prime'], row['s'], row['a'], row['t']),
+                        return_dtype=pl.Float32,
+                    ).alias('probability'),
+                    pl.struct('s_prime', 's', 'a', 't').map_elements(
+                        lambda row: self.reward_function(row['s_prime'], row['s'], row['a'], row['t']),
+                        return_dtype=pl.Float32,
+                    ).alias('reward'),                    
+                ])
+
+                df_a = df_a.collect()
+
+            if self.mode == 'vectorized':
+                # Use the apply method to apply the transition and reward
+                # functions
+                #TODO: How can we do this without a collect operation?
+                df_a = df_a.collect()
+                df_a = df_a.with_columns([
+                    self.transition_function(df_a['s_prime'], df_a['s'], df_a['a'], df_a['t']).alias('probability'),
+                    self.reward_function(df_a['s_prime'], df_a['s'], df_a['a'], df_a['t']).alias('reward'),
+                ])
+
+            #-----------------------------------------------------------------
+            # Unclear if this works until tested
+            if self.mode == 'model':
+                df_a = df_a.collect()
+
+                X = df_a[self.model.feature_names_]
+                probabilities = self.model.predict_proba(X)
+                probabilities = np.round(probabilities, 4)
+
+                # Add a column called s_prime
+                df_a['s_prime'] = [self.states] * len(df_a)
+
+                # Add the probabilities
+                df_a['probability'] = probabilities.tolist()
+
+                # Now we need to explode the probability column
+                df_a = df_a.explode(['probability', 's_prime'])
+
+                # Now filter out any rows where probability is within 1e-4 of 0
+                df_a = df_a.query("probability > 1e-4")
+
+                # Compute reward
+                df_a.loc[:, 'reward'] = self.reward(df_a['s_prime'], df_a['s'], df_a['t'], df_a['a'])
+
+            #-----------------------------------------------------------------
+            # Add a columns t_prime which is t + 1
+            df_a = df_a.with_columns([
+                pl.lit(df_a['t'] + 1).alias('t_prime')
+            ])
+
+            # Only retain columns where t_prime <= max(times)
+            df_a = df_a.filter(pl.col('t_prime') <= max(self.times))
+
+            # Contruct the transition and reward matrices
+            P, R = self.__build_matrix(df_a)
+            
+            transitions.append(P)
+            rewards.append(R)
+
+        self.transition_matrices = transitions
+        self.reward_matrices = rewards
+        
+        return None
+                
+
+    def _enforce_valid_matrices(self):
+        """
+        Sometimes the transition matrices don't have rows summing to 1. This is
+        either because there are inaccessible states, or there are rounding
+        errors.
+
+        First check for rows with all zeros. If there are any, set the diagonal
+        to 1 and set the corresponding entry in rewards to INFEASIBLE.
+
+        Then, check for rows that don't sum to 1. If there are any, normalize
+        the row so that it sums to 1.
+        """
+
+        # If transitions or rewards are an empty list, throw an error
+        if not self.transition_matrices or not self.reward_matrices:
+            raise ValueError("Transitions and rewards must be non-empty lists")
+
+        new_transitions = []
+        new_rewards = []
+
+        for P, R in zip(self.transition_matrices, self.reward_matrices):
+            # Sum the rows of the transition matrix
+            row_sums = P.sum(axis=1)
+
+            # Find where the row sums are zero
+            zero_rows = np.where(row_sums == 0)[0]
+
+            # Construct a new csr matrix with the same shape as the original, but
+            # with a 1 on the diagonal where the row sums are zero
+            adjustment_P = csr_matrix(
+                (np.ones_like(zero_rows), (zero_rows, zero_rows)),
+                shape=P.shape,
+                dtype=float,
+            )
+
+            # Construct a new reward matrix with infeasible rewards where the row
+            # sums are zero
+            adjustment_R = csr_matrix(
+                (
+                    np.ones_like(zero_rows) * self.INFEASIBLE,
+                    (zero_rows, zero_rows),
+                ),
+                shape=P.shape,
+                dtype=float,
+            )
+
+            new_P = P + adjustment_P
+            new_R = R + adjustment_R
+
+            # Normalize so rows sum to 1
+            new_P = preprocessing.normalize(new_P, norm="l1", axis=1)
+
+            # Add the new matrices to the list
+            new_transitions.append(new_P)
+            new_rewards.append(new_R)
+
+        self.transition_matrices = new_transitions
+        self.reward_matrices = new_rewards
+
+        return None
+    
+
+    def solve(self):
+        self.build_rewards_and_transitions()
+        self._enforce_valid_matrices()
+
+        n = len(self.times)
+
+        # Solve the finite horizon MDP
+        mdp = FiniteHorizon(
+            self.transition_matrices,
+            self.reward_matrices,
+            1,
+            n,
+        )
+
+        mdp.run()
+
+        # ---------------------------------------------------------------------#
+        # The value function has an entry for each (s,t) in S_augmented.
+        # We need to filter down to the entries where t is not the dummy time
+        # period
+
+        self.value_function = mdp.V
+        self.policy_function = mdp.policy
+
+        self._extract_feasible_states()
+
+        logger.info("MDP solved")
+
+        return None
+
+    def _extract_feasible_states(self) -> None:
+        """
+        The value function will be of shape (len(S_augmented), len(T)).
+
+        Note that the final time period is a dummy time period.
+
+        Rows are states, columns are time periods.
+
+        We want to create a dictionary where the keys are times, and the values are
+        maps of states to actions. Only states where the time value equals the
+        current time are feasible.
+        """
+
+        policy, value = self.policy_function, self.value_function
+
+        # Correct the shape
+        policy = policy[:, 1:]
+        value = value[:, 1:-1]
+
+        policy_dict = {}
+        value_dict = {}
+
+        # Policy first
+        # Check that at least one of policy or value is not None
+        if policy is None and value is None:
+            raise ValueError(
+                "At least one of policy or value must be not None."
+            )
+
+        # Check that shapes are correct.
+        if policy is not None:
+            if (
+                policy.shape[0] != len(self.states_augmented)
+                or policy.shape[1] != len(self.times) - 1
+            ):
+                logger.error(
+                    f"Policy shape is incorrect. Expected shape {(len(self.states_augmented), len(self.times) - 1)}, got {policy.shape}"
+                )
+                raise ValueError("Policy shape is incorrect.")
+
+        if (
+            value.shape[0] != len(self.states_augmented)
+            or value.shape[1] != len(self.times) - 1
+        ):
+            logger.error(
+                f"Value shape is incorrect. Expected shape {(len(self.states_augmented), len(self.times) - 1)}, got {value.shape}"
+            )
+            raise ValueError("Value shape is incorrect.")
+
+        for time_ix, time in enumerate(self.times[:-1]):
+            time_dict_value = {}
+            time_dict_policy = {}
+            for state_ix, state in enumerate(self.states_augmented):
+                # If the time index is the same as the current time, add the state to the policy dict.
+                if state[1] == time:
+                    time_dict_value[state[0]] = float(value[state_ix, time_ix])
+                    time_dict_policy[state[0]] = self.actions[
+                        policy[state_ix, time_ix]
+                    ]
+
+            value_dict[time] = time_dict_value
+            policy_dict[time] = time_dict_policy
+
+        self.policy_function = policy_dict
+        self.value_function = value_dict
+
+        return None
+
+
+    def get_transition_probabilties(
+        self,
+        s: int | str,
+        t: int,
+        a: int | str,
+    ) -> dict[str, Any]:
+        """
+        Plot the transition probabilities at time step t for action a.
+        """
+
+        # Checks on the inputs
+        assert s in self.states, "State must be a valid value in S"
+
+        # Check t is a valid value for time
+        assert t in self.times, "Time must be a valid value in T"
+
+        # Check that t is not the last time period
+        assert t != max(self.times), (
+            "Cannot plot transition probabilities for the last time period"
+        )
+
+        # Check a is a valid action
+        assert a in self.actions, "Action must be a valid action in A"
+
+        # What is the index of a?
+        a_ix = self.actions.index(a)
+
+        # Get the transition matrix for action a
+        P = self.transition_matrices[a_ix]
+        R = self.reward_matrices[a_ix]
+
+        # Which row of the transition matrix corresponds to state s at time t?
+        s_current = [
+            k
+            for k, v in enumerate(self.states_augmented)
+            if v[0] == s and v[1] == t
+        ]
+
+        # Check that this has exactly one element and extract it
+        assert len(s_current) == 1, "State s at time t must be unique"
+        s_current_ix = s_current[0]
+
+        # For S_augmented, find which states have time index t_ix + 1
+        s_prime_ix = [
+            k for k, v in enumerate(self.states_augmented) if v[1] == t + 1
+        ]
+        s_prime_labels = [self.states_augmented[ix][0] for ix in s_prime_ix]
+
+        # Pull out the s_current_ix row of the transition matrix and the s_prime_ix columns
+        probs = P[s_current_ix, :].toarray().flatten()[s_prime_ix]
+
+        rewards = R[s_current_ix, :].toarray().flatten()[s_prime_ix]
+
+        return {
+            "current_state": s,
+            "current_time": t,
+            "action": a,
+            "next_states": s_prime_labels,
+            "probabilities": probs,
+            "rewards": rewards,
+        }
+
+    def plot_matrix(
+        self,
+        matrix_type: str,
+        t: int,
+        a: int | str,
+        ax: plt.Axes | None = None,
+        annotate: bool = False,
+        vmin: float | None = None,
+        vmax: float | None = None,
+    ) -> None:
+        """
+        Plot the transition probabilities at time step t for action a.
+
+        Use a seaborn heatmap where the y axis is s and the x axis is s_prime.
+        """
+
+        assert matrix_type in ["transitions", "rewards"], (
+            "Type must be either 'transitions' or 'rewards'"
+        )
+        assert t in self.times, "Time must be a valid value in T"
+        assert a in self.actions, "Action must be a valid action in A"
+
+        # empty np array
+        matrix = np.zeros((len(self.states), len(self.states)))
+
+        y_labels = self.states
+
+        for i, s in enumerate(y_labels):
+            data = self.get_transition_probabilties(s, t, a)
+            if matrix_type == "transitions":
+                matrix[i, :] = data["probabilities"]
+            if matrix_type == "rewards":
+                matrix[i, :] = data["rewards"]
+
+        if ax is None:
+            fig, ax = plt.subplots(figsize=(10, 6))
+        else:
+            fig = ax.get_figure()
+
+        # Plot the heatmap
+        sns.heatmap(
+            matrix,
+            xticklabels=y_labels,
+            yticklabels=y_labels,
+            ax=ax,
+            annot=annotate,
+            linewidths=0.5,
+            linecolor="black",
+            cmap="coolwarm",
+            alpha=0.7,
+            vmin=vmin,
+            vmax=vmax,
+        )
+        ax.set_xlabel("Next State")
+        ax.set_ylabel("Current State")
+        ax.set_title(f"{matrix_type.title()} at Time {t} for Action {a}")
+
+        # Reduce the number of labels to at most 5
+        max_labels = 5
+        if max_labels is not None:
+            x_labels = ax.get_xticks()
+            y_labels = ax.get_yticks()
+            ax.set_xticks(x_labels[:: max(1, len(x_labels) // max_labels)])
+            ax.set_yticks(y_labels[:: max(1, len(y_labels) // max_labels)])
+
+        return None
