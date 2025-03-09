e075f40ca094ba482ccda52a8a14a6c4c3367729
Brad Pfeil <Brad-Pfeil@users.noreply.github.com>
2025-03-09 14:20:11 +1030

    feat: infer mode from whether reward and transitions are vectorized functions

--- src/markov_decision_process/time_augmented_mdp.py
+++ src/markov_decision_process/time_augmented_mdp.py
@@ -31,7 +31,7 @@ class TimeAugmentedMDP:
         reward_function: Callable,
         transition_function: Callable | None = None,
         discount_factor: float = 1,
-        mode: Literal['flexible', 'vectorized', 'model'] = 'flexible',
+        mode: Literal['flexible', 'vectorized', 'model'] | None = None,
         model: Callable | None = None,
     ):
 
@@ -44,9 +44,19 @@ class TimeAugmentedMDP:
         self.transition_matrices: List[csr_matrix] = []
         self.reward_matrices: List[csr_matrix] = []
        
-
+        # If model is passed, set the model to be model
+        if self.model is not None:
+            logger.info("Model provided. Setting mode to 'model'")
+            self.mode = 'model'
+        
         # Checks on inputs
         self.__check_inputs()
+
+        # If the mode is not set, try to infer it.
+        if self.mode is None:
+            logger.info('Mode not set. Inferring mode...')
+            self.__infer_mode()
+
 
         return None
 
@@ -81,14 +91,17 @@ class TimeAugmentedMDP:
             raise ValueError('Times must be consecutive and increasing')
         
         # Mode must be one of the allowed values
-        if self.mode not in ['flexible', 'vectorized', 'model']:
-            raise ValueError('Mode must be one of "flexible", "vectorized", or "model"')
+        if self.mode is not None:
+            if self.mode not in ['flexible', 'vectorized', 'model']:
+                raise ValueError('Mode must be one of "flexible", "vectorized", or "model"')
         # If mode is 'model', model must be a function
         if self.mode == 'model' and not callable(self.model):
             raise ValueError('If mode is "model", model must be a function')
         # If mode is flexible or vectorized, a transition function must be provided
         if self.mode in ['flexible', 'vectorized'] and not callable(self.transition_function):
             raise ValueError('If mode is "flexible" or "vectorized", a transition function must be provided')
+        if self.mode is None and not callable(self.transition_function):
+            raise ValueError('Either a model or a transition function must be provided')
         
         # Check that reward function has correct arguments
         reward_signature = list(inspect.signature(self.reward_function).parameters.keys())
@@ -102,5 +115,50 @@ class TimeAugmentedMDP:
             if transition_signature != ['s_prime', 's', 'a', 't']:
                 logger.warning(f'Transition function currently has arguments {transition_signature}. Should be (s_prime, s, a, t)')
                 raise ValueError('Transition function should have arguments (s_prime, s, a, t)')
+        
+        return None
+
+    def __is_func_vectorized(self, func):
+        # Create test series inputs
+        s_prime = pd.Series([1, 2, 3])
+        s = pd.Series([1, 2, 3])
+        a = pd.Series([1, 2, 3])
+        t = pd.Series([1, 2, 3])
+        
+        
+        try:
+            # Try to apply the function to the series inputs
+            result = func(s_prime, s, a, t)
+            
+            # Check if the result is a pd.Series
+            if isinstance(result, pd.Series):
+                return True
+            else:
+                return False
+        except Exception as e:
+            # If an exception occurs, it's likely not vectorized
+            return False
+        
+    def __infer_mode(self):
+        """
+        Checks if the reward and transition functions are vectorized. If they
+        are, sets mode to 'vectorized'. Otherwise, sets mode to 'flexible'.
+        """
+        
+        # Is reward vectorized 
+        reward_vectorized = self.__is_func_vectorized(self.reward_function)
+        logger.info(f'Reward function is vectorized: {reward_vectorized}')
+
+        # Only inferring mode if we don't already have a model passed.
+        # So transition function must exist.
+        transition_vectorized = self.__is_func_vectorized(self.transition_function)
+        logger.info(f'Transition function is vectorized: {transition_vectorized}')
+
+        if reward_vectorized and transition_vectorized:
+            logger.info('Reward and transition functions are vectorized. Setting mode to "vectorized"')
+            self.mode = 'vectorized'
+        else:
+            logger.info('Reward and transition functions are not vectorized. Setting mode to "flexible"')
+            self.mode = 'flexible'
         
         return None
